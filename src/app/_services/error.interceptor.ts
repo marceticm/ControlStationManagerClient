e5ffd3ab86b41bceafa7edb877eec4e0a0f82e4f
Milan Marcetic <milan.marcetic@startelecom.ca>
2020-08-28 11:28:23 +0200

    - added alertifyjs - created and utilized a service for popup notifications - improved error handling in error interceptor

--- src/app/_services/error.interceptor.ts
+++ src/app/_services/error.interceptor.ts
@@ -17,7 +17,7 @@ export class ErrorInterceptor implements HttpInterceptor {
                         console.error(applicationError);
                         return throwError(applicationError);
                     }
-                    const serverError = error.error;
+                    const serverError = error.error.errors;
                     let modalStateErrors = '';
                     if (serverError && typeof serverError === 'object') {
                         for (const key in serverError) {
--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../_services/auth.service';
+import { AlertifyService } from '../_services/alertify.service';
 
 @Component({
   selector: 'app-nav',
@@ -9,15 +10,15 @@ import { AuthService } from '../_services/auth.service';
 export class NavComponent implements OnInit {
   model: any = {};
 
-  constructor(private authService: AuthService) { }
+  constructor(private authService: AuthService, private alertify: AlertifyService) { }
 
   ngOnInit() {
   }
 
   login() {
     this.authService.login(this.model).subscribe({
-      next: x => console.log('Logged in successfully'),
-      error: err => console.log(err)
+      next: x => this.alertify.success('Logged in successfully'),
+      error: err => this.alertify.error(err)
     });
   }
 
@@ -28,7 +29,7 @@ export class NavComponent implements OnInit {
 
   logout() {
     localStorage.removeItem('token');
-    console.log('logged out');
+    this.alertify.message('Logged out');
   }
 
 }
