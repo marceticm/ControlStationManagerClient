a4be3a755439733798e749cb953d5269dc9f3c14
Milan Marcetic <marcetic18@hotmail.com>
2020-08-28 16:27:44 +0200

    added ngx bootstrap to improve some styling

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 
-import { AppComponent } from './app.component';
 import { DxDataGridModule } from 'devextreme-angular';
-import { NavComponent } from './nav/nav.component';
 import { FormsModule } from '@angular/forms';
+import { BsDropdownModule } from 'ngx-bootstrap';
+
+import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
 import { AuthService } from './_services/auth.service';
 import { HomeComponent } from './home/home.component';
 import { RegisterComponent } from './register/register.component';
@@ -23,7 +25,8 @@ import { AlertifyService } from './_services/alertify.service';
     BrowserModule,
     HttpClientModule,
     DxDataGridModule,
-    FormsModule
+    FormsModule,
+    BsDropdownModule.forRoot()
   ],
   providers: [
     AuthService,
