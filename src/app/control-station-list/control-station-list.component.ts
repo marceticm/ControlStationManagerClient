ff4dd465e276971927613285082dfb5ef08e7841
Milan Marcetic <milan.marcetic@startelecom.ca>
2020-09-07 15:02:24 +0200

    improved datagrid look and functionality

--- src/app/control-station-list/control-station-list.component.ts
+++ src/app/control-station-list/control-station-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, AfterViewInit, ViewChild, OnInit } from '@angular/core';
 import { ControlStationService } from '../_services/control-station.service';
 import { ControlStation } from '../_models/control-station';
 import { AlertifyService } from '../_services/alertify.service';
@@ -9,14 +9,48 @@ import { DxDataGridComponent } from 'devextreme-angular';
   templateUrl: './control-station-list.component.html',
   styleUrls: ['./control-station-list.component.css']
 })
-export class ControlStationListComponent implements OnInit {
+export class ControlStationListComponent implements OnInit, AfterViewInit {
+  @ViewChild('targetDataGrid', { static: false }) dataGrid: DxDataGridComponent;
+
   controlStations: ControlStation[];
-  dataGrid: DxDataGridComponent;
 
   constructor(private stationService: ControlStationService, private alertify: AlertifyService) { }
 
   ngOnInit() {
     this.loadControlStations();
+  }
+
+  ngAfterViewInit() {
+    this.dataGrid.instance.option({
+      allowColumnReordering: true,
+      allowColumnResizing: true,
+      columnChooser: {
+        enabled: true,
+        allowSearch: true,
+        mode: "select",
+        heigh: 400
+      },
+      columnAutoWidth: true,
+      rowAlternationEnabled: true,
+      filterRow: {
+        visible: true
+      },
+      paging: {
+        enabled: true,
+        pageSize: 20
+      },
+      pager: {
+        showPageSizeSelector: true,
+        showNavigationButtons: true,
+        allowedPageSizes: [5, 10, 20, 50],
+        showInfo: true,
+        visible: true
+      },
+      searchPanel: { visible: true },
+      selection: {
+        mode: "single"
+      },
+    })
   }
 
   loadControlStations() {
